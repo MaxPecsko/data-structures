4ca761cc459892af68a3f265ce4350bff6adc2f6
MakszimPecsko <mk.pecsko@gmail.com>
2020-09-14 23:22:09 +0300

    DLL: pop, shift, unshift, get

--- DataStructures/DoublyLinkedList.js
+++ DataStructures/DoublyLinkedList.js
@@ -28,4 +28,72 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.length) return undefined;
+
+    const oldTail = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      oldTail.prev = null;
+    }
+
+    this.length--;
+    return oldTail;
+  }
+
+  shift() {
+    if (!this.length) return undefined;
+
+    const shiftedHead = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = shiftedHead.prev;
+      this.head.prev = null;
+      shiftedHead.next = null;
+    }
+
+    this.length--;
+    return shiftedHead;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+
+    if (!this.length) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    if (index <= this.length/2) {
+      for (let i = this.head, j = 0;j <= index;j++, i = i.next) {
+        if (j === index) return i;
+      }
+    }
+
+    if (index >= this.length/2) {
+      for (let i = this.tail, j = this.length-1;j >= index;j--, i = i.prev) {
+        if (j === index) return i;
+      }
+    }
+  }
 }
